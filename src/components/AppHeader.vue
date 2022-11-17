6835480cc6183657a01900a7b405b19557e46d4e
ImBlindForU <samuele.zanchi10@gmail.com>
2022-11-17 15:58:47 +0100

    inizio header base non dinamico e implementazione jumbotron

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,13 +7,46 @@ export default{
 
 <template>
 
-    <div class="container-fluid">
+    <div class="container-fluid bg-color-h">
+        <!-- general bar -->
+        <div class="container flex space-between">
+            <div class="logo">
+                <img src="" alt="">
+                logo
+            </div>
+            <div class="list-general ">
+                <ul class="flex">
+                    <li><a href="">Home</a> </li>
+                    <li><a href="">Home</a> </li>
+                    <li><a href="">Home</a> </li>
+                    <li><a href="">Home</a> </li>
+                    <li><a href="">Home</a> </li>
+                    <li><a href="">Home</a> </li>
+                </ul>
+            </div>
+            <div class="icons">
+                icons
+            </div>
+        </div>
+        <!-- jumbotron -->
         <div class="container">
-            <h2>ciao</h2>
+            <div class="jumbo-text">
+                <h2>aaaaa</h2>
+                <h1>wwellallalalalalalal</h1>
+                <p>asddasdad</p>
+                <button>ciao</button>
+            </div>
+            <div class="jumbo-img">
+                
+            </div>
         </div>
     </div>
 
 </template>
 
 <style lang="scss" scoped>
+
+.bg-color-h{
+    background-color: #F6F6FC;
+}
 </style>
